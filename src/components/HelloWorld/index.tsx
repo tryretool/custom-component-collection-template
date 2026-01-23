5d0ec9be2c4dfbd81cd365c2fec62a985aa87bbf
Michael Häusler <michael.haeusler@researchgate.net>
2026-01-23 13:07:47 +0100

    feat: modular template for multiple components, bidirectional databinding, and unit testing

---
+++ src/components/HelloWorld/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { type FC } from 'react'
+import { Retool } from '@tryretool/custom-component-support'
+
+export const HelloWorld: FC = () => {
+    const [name, _setName] = Retool.useStateString({
+        name: 'name'
+    })
+
+    return (
+        <div>
+            <div>Hello {name}!</div>
+        </div>
+    )
+}
